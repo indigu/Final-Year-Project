616dab39ea4376543e86d06f86823a97541c21b8
indigu <c15477298@mydit.ie>
2019-02-05 19:47:33 +0000

    More work on Registration
    Adding functionalities on Registration

--- SignApp/app/src/main/java/com/example/sign_app/Activities/RegisterActivity.java
+++ SignApp/app/src/main/java/com/example/sign_app/Activities/RegisterActivity.java
@@ -1,15 +1,63 @@
 package com.example.sign_app.Activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.sign_app.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView userAvatar;
+    static int PReqCode = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // inu views
+        userAvatar = findViewById(R.id.regAvatar);
+
+        userAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkAndRequestPermission();
+                }
+                else {
+                    openGallery();
+                }
+            }
+        });
     }
+
+    private void openGallery() {
+    }
+
+    private void checkAndRequestPermission() {
+
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(RegisterActivity.this, "The app requires access to the storage to work", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                                    PReqCode);
+            }
+
+        }
+    }
+
+
 }

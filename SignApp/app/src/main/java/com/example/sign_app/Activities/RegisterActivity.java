bdcea61b86405ae6ef59fadc22bf315392f77577
indigu <c15477298@mydit.ie>
2019-02-05 19:59:13 +0000

    Avatar Functionality
    User can now pick his/her own avatar picture

--- SignApp/app/src/main/java/com/example/sign_app/Activities/RegisterActivity.java
+++ SignApp/app/src/main/java/com/example/sign_app/Activities/RegisterActivity.java
@@ -2,8 +2,11 @@ package com.example.sign_app.Activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +17,14 @@ import android.widget.Toast;
 
 import com.example.sign_app.R;
 
+import java.net.URI;
+
 public class RegisterActivity extends AppCompatActivity {
 
     ImageView userAvatar;
     static int PReqCode = 1;
+    static int RequestCode = 1;
+    Uri chosenImgUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +48,14 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void openGallery() {
+        // for older phones
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, RequestCode);
     }
 
     private void checkAndRequestPermission() {
-
+        // for newer phones
         if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
             if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
@@ -55,9 +66,22 @@ public class RegisterActivity extends AppCompatActivity {
                                                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                                                     PReqCode);
             }
-
+        }
+        else{
+            openGallery();
         }
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == RequestCode && data != null){
+            // this signals the user successfully picking an image
+            // we then save its reference to a Uri variable
+            chosenImgUri = data.getData();
+            userAvatar.setImageURI(chosenImgUri);
+        }
+    }
 }
